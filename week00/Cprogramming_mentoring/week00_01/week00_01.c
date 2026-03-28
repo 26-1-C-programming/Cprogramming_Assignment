35d9035c66b496bc8883152e2268d062a1ea0b75
jingbab <sjjlsup611@gmail.com>
2026-03-28 23:59:38 +0900

    이서준:week00 assignment

---
+++ week00/Cprogramming_mentoring/week00_01/week00_01.c
@@ -0,0 +1,19 @@
+﻿#include <stdio.h>
+int main(void)
+{
+	int first, second, third, fourth;
+	printf("첫 번째 계산할 값을 입력하세요 ==>");
+	scanf_s("%d", &first);
+
+	printf("두 번째 계산할 값을 입력하세요 ==>");
+	scanf_s("%d", &second);
+
+	printf("세 번째 계산할 값을 입력하세요 ==>");
+	scanf_s("%d", &third);
+
+	printf("네 번째 계산할 값을 입력하세요 ==>");
+	scanf_s("%d", &fourth);
+
+	printf("%d + %d + %d + %d = %d", first, second, third, fourth, first + second + third + fourth);
+
+}
---
+++ week00/Cprogramming_mentoring/week00_02/week00_02.c
@@ -0,0 +1,12 @@
+﻿#include <stdio.h>
+
+int main() {
+    int year;
+
+    printf("연도를 입력하세요: ");
+    scanf_s("%d", &year);
+
+    
+    printf("%d년은 %s", year, ( (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0) ) ? "윤년입니다." : "윤년이 아닙니다.");
+    return 0;
+}
