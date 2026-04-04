433932987ab00a5b9598537ab54fd60ad86c060c
laizpe7-cmd <Laizpe7@gmail.com>
2026-04-04 11:35:44 +0900

    {1주차_추가문제}

---
+++ Cprograming/week00_05/week00_05.c
@@ -0,0 +1,24 @@
+﻿//[5번 문제] 입력된 문자열을 거꾸로 출력
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void)
+{
+
+	char str[10];
+	int len;
+
+	printf("문자열을 입력 ==> ");
+	scanf("%s", &str);
+
+	for (len = 0; str[len] != '\0'; len++);
+
+	while (len > 0) {
+		printf("%c", str[len - 1]);
+		len--;
+	}
+
+	return 0;
+
+}
