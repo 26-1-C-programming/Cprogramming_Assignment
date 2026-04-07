e8db77892a44a07b2341922ecee35e75a3bb3aa7
gangha06 <ganghaha06@gmail.com>
2026-04-07 17:18:25 +0900

    김강하:1주차 과제

---
+++ week02/week02_04/week02_03/week02_03.c
@@ -0,0 +1,27 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+	int a;
+	int b;
+	int c;
+
+	printf("무슨 연산? 덧셈 = 1, 뺄셈 = 2, 곱셈 = 3, 나눗셈 = 4 ==> ");
+	scanf("%d", &a);
+	printf("연산할 두 수 입력: \n");
+	scanf("%d %d", &b, &c);
+
+	if (a == 1)
+		printf("%d", b + c);
+	else if (a == 2)
+		printf("%d", b - c);
+	else if (a == 3)
+		printf("%d", b * c);
+	else if (a == 4)
+		printf("%f", b / c);
+	else
+		printf("다시입력");
+
+	return 0;
+}
